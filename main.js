804634c9a94308521d0c6c4809c7155064f76373
hongwoo0516-commits <hongwoo0516@gmail.com>
2026-03-17 10:32:30 +0900

    1st

---
+++ main.js
@@ -0,0 +1,22 @@
+document.getElementById('time').innerHTML = new Date().toLocaleString()
+
+function pink() {
+  document.body.style.backgroundcolor = 'pink'
+}
+function Lightblue() {
+  document.body.style.backgroundcolor = 'Lightblue'
+}
+function yellow() {
+  document.body.style.backgroundcolor = 'yellow'
+}
+function reset() {
+  document.body.style.backgroundcolor = 'line'
+}
+function showhtml() {
+  document.getElementById('fig').src = 'img/다운로드.png'
+  document.getElementById('desc').innerHTML = 'HTML은 구조적 웹'
+  document.getElementById('fig').src = 'img/java사진(1).png'
+  document.getElementById('desc').innerHTML = 'HTML은 구조적 웹'
+  document.getElementById('fig').src = '다운로드.png'
+  document.getElementById('desc').innerHTML = 'HTML은 구조적 웹'
+}
